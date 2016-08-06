6fc308d3858b48f67749291c9c529b516afc1dbd
Ant <akongx@gmail.com>
2016-08-06 02:55:02 -0700

    add some simple css for the chat list, setup chat list links to go to chat detail

--- app/list/list.js
+++ app/list/list.js
@@ -10,44 +10,46 @@ angular.module('chat.list', ['ngRoute'])
 }])
 
 .controller('ListCtrl', ['$scope', '$routeParams', 'DataService', function($scope, $routeParams, DataService) {
-  var uid = $routeParams.id ? $routeParams.id : 0;
+  var uid = $routeParams.id ? parseInt($routeParams.id, 10) : 0;
   $scope.userChats = [];
+  if(!isNaN(uid)) {
 
-  // chain the requests because userData is needed
-  DataService.getUsers().then(function(userData) {
+    // chain the requests because user data is needed
+    DataService.getUsers().then(function(userData) {
 
-    var getUserById = function(id) {
-      var c = userData.length - 1;
-      id = parseInt(id, 10);
-      if(isNaN(id)) return;
-      do {
-        if(userData[c].id === id) {
-          return userData[c];
-        }
-      } while(c--);
-      return null;
-    };
-
-    DataService.getChats().then(function(chatData) {
-      var chats = chatData;
-      for(var i = 0; i < chats.length; i++) {
-        var foundIdx = chats[i].participants.indexOf(uid);
-        if(foundIdx >= 0) {
-          // remove the user
-          chats[i].participants.splice(foundIdx, 1);
-
-          // replace participant id's with participant names
-          var pIds = chats[i].participants;
-          var pNames = [];
-          for(var j = 0; j < pIds.length; j++) {
-            pNames.push(getUserById(pIds[j]));
+      var getUserById = function(id) {
+        var c = userData.length - 1;
+        id = parseInt(id, 10);
+        if(isNaN(id)) return;
+        do {
+          if(userData[c].id === id) {
+            return userData[c];
           }
-          chats[i].participants = pNames;
+        } while(c--);
+        return null;
+      };
 
-          $scope.userChats.push(chats[i]);
-        }
-      } 
+      DataService.getChats().then(function(chatData) {
+        var chats = chatData;
+        for(var i = 0; i < chats.length; i++) {
+          var foundIdx = chats[i].participants.indexOf(uid);
+          if(foundIdx >= 0) {
+            // remove the user
+            chats[i].participants.splice(foundIdx, 1);
+
+            // replace participant id's with participant names
+            var pIds = chats[i].participants;
+            var pNames = [];
+            for(var j = 0; j < pIds.length; j++) {
+              pNames.push(getUserById(pIds[j]));
+            }
+            chats[i].participants = pNames;
+
+            $scope.userChats.push(chats[i]);
+          }
+        } 
+      });
     });
-  });
+  }
 
 }]);

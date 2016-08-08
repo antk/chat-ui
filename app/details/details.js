384653e41734b173686bc65d07b942873c8f0764
Ant <akongx@gmail.com>
2016-08-07 23:46:05 -0700

    insert message functionality

--- app/details/details.js
+++ app/details/details.js
@@ -14,6 +14,16 @@ angular.module('chat.details', ['ngRoute'])
     $scope.chatId = parseInt($routeParams.id, 10);
     $scope.userId = parseInt($routeParams.uid, 10);
     if(!isNaN($scope.chatId) && !isNaN($scope.userId)) {
+      $scope.messages = [];
+      $scope.newMessage = "";
+      $scope.insertMessage = function(sender_id) {
+        var msgId = $scope.messages[$scope.messages.length-1].msg_id + 1;
+        var msg = {"msg_id":msgId, "sender_id":sender_id, "text":$scope.newMessage, "datetime":""}
+        console.log(msg);
+        $scope.messages.push(msg);
+        $scope.newMessage = "";
+      }
+
       DataService.getChatById($scope.chatId).then(function(data) {
         console.log(data);
         var theParticipants = [];
